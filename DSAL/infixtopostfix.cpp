b0891069e2b880a74a67f2d06809d1c9bd2b0e6a
Adwait Nagarkar <109037318+Adwait47s@users.noreply.github.com>
2022-09-29 10:55:54 +0530

    Create infixtopostfix.cpp

---
+++ DSAL/infixtopostfix.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <stack>
+using namespace std;
+int main(){
+    cout << endl;
+    cout << "Please enter you expression without spaces " << endl;
+    string s;
+    cin>>s;
+    stack<char> ss;
+    cout << endl;
+    cout << "Postfix Expression: ";
+    for(int i=0;i<s.size();i++){
+        if(s[i]>=97){
+            cout << s[i];
+        }
+        else{
+            if(ss.empty()){
+                ss.push(s[i]);
+            }
+            else{
+                if(s[i]=='('){
+                    ss.push(s[i]);
+                }
+                else if(s[i]==')'){
+                    while(ss.top()!='('){
+                        cout << ss.top();
+                        ss.pop();
+                    }
+                    ss.pop();
+                }
+                else if(s[i]=='+'||s[i]=='-'){
+                    while(!ss.empty()){
+                        if(ss.top()=='('){
+                            break;
+                        }
+                        cout << ss.top();
+                        ss.pop();
+                    }
+                    ss.push(s[i]);
+                }
+                else if(s[i]=='*'||s[i]=='/'){
+                    while(!ss.empty()){
+                        if(ss.top()=='('){
+                            break;
+                        }
+                        if(ss.top()=='+'||ss.top()=='-'){
+                            break;
+                        }
+                        cout << ss.top();
+                        ss.pop();
+                    }
+                    ss.push(s[i]);
+                }
+            }
+        }
+    }
+    
+    if(!ss.empty()){
+        while(!ss.empty()){
+            cout << ss.top();
+            ss.pop();
+        }
+    }
+    cout << endl;
+    cout << endl;
+}
