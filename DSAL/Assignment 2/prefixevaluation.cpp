5b98f33a7a9bdb1558558d8a0025a9a46fb3ef84
Adwait Nagarkar <109037318+Adwait47s@users.noreply.github.com>
2022-10-06 10:08:42 +0530

    Create prefixevaluation.cpp

---
+++ DSAL/Assignment 2/prefixevaluation.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << endl;
+    cout << "Please enter prefix expression in the form of variables : ";
+    string s; cin>>s;
+    map<char,double>m;
+    for(int i=0;i<s.size();i++){
+        if(s[i]>=65){
+            m[s[i]] = 0;
+        }
+    }
+    cout << "Please enter values for variables" << endl;
+    for(auto it:m){
+        cout << it.first << " = ";
+        double a; cin>>a;
+        m[it.first] = a;
+    }
+    stack<double>ss;
+    for(int i=s.size()-1;i>=0;i--){
+        if(s[i]>=65){
+            ss.push(m[s[i]]);
+        }
+        else{
+            double first,second,newadd,value1,value2;
+            char expression,v1,v2;
+            second = ss.top();
+            ss.pop();
+            first = ss.top();
+            ss.pop();
+            expression = s[i];
+            if(expression=='+'){
+                newadd = first +second;
+            }
+            else if(expression=='-'){
+                newadd = first-second;
+            }
+            else if(expression=='*'){
+                newadd = first*second;
+            }
+            else if(expression=='/'){
+                newadd = first/second;
+            }
+            ss.push(newadd);
+        }
+    }
+    cout << endl;
+    cout << "Value of prefix expression : ";
+    cout << ss.top()<< endl;
+    cout << endl;
+}
